fb3c5103f76ef6b4349435b937f4d3102ae0daa6
Selene <jessre27@gmail.com>
2026-02-09 19:37:51 -0600

    Header: Add shopping cart button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
 import logo from "../images/logo.png";
 import carrito from "../images/carrito.png";
-import "../blocks/header.css";
 
 export default function Header() {
   return (
-    <header className="header">
-      {/* <img src={logo} className="header__logo" /> */}
-      <h1>CAPU</h1>
-      <img src={carrito} className="header__carrito" alt="carrito de compras" />
+    <header className="w-full bg-white shadow-sm">
+      <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-4">
+        <div className="flex items-center gap-2">
+          <img src={logo} alt="imagen de abeja" className="h-20 w-auto px-6" />
+          <h1 className="text-xl font-semibold px-2 tracking-wide">
+            ORDENA TUS BEBIDAS
+          </h1>
+        </div>
+
+        <div className="flex items-center gap-3 px-8">
+          <button className="flex items-center gap-4 rounded-full bg-orange-500 px-4 py-2 text-sm font-medium text-white shadow hover:bg-orange-600 transition">
+            <img src={carrito} alt="carrito de compras" className="h-10 w-10" />
+            <span className="text-xl px-1">0</span>
+          </button>
+        </div>
+      </div>
     </header>
   );
 }
